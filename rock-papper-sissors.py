30be2facd9e2892874fba8caf8e573ab96c94c9e
gbrough <gbrough@gmail.com>
2021-06-21 22:00:30 -0700

    switch if else statement

--- rock-papper-sissors.py
+++ rock-papper-sissors.py
@@ -9,10 +9,10 @@ user = input('choose "r" for rock, "p" for paper, "s", for sissors \n').lower()
 options = ['r', 'p', 's']
 machine = random.choice(options)
 
-if machine == "r" and user == "s" or machine == "p" and user == "r" or machine == "s" and user == "p":
-  print("The machine won")
+if machine == user:
+  print("It's a tie")
 elif user == "r" and machine == "s" or user == "p" and machine == "r" or user == "s" and machine == "p":
   print("Yay! You win!")
-elif machine == user:
-  print("It's a tie")
+else:
+  print("Sorry, the machine won")
 
